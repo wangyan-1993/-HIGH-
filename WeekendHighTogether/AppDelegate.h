ed9f6157dad122bc137f198112537139da6d619c
王妍 <843668546@qq.com>
2016-01-04 11:23:22 +0800

    first

---
+++ WeekendHighTogether/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeekendHighTogether
+//
+//  Created by SCJY on 16/1/4.
+//  Copyright © 2016年 SCJY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
